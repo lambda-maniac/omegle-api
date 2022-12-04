9342df80cba81a93560ee15168fec2bb704b7071
Sr. Meia-noiteλ <lordzarkares@Outlook.com>
2022-12-04 06:02:17 -0300

    |

--- OmegleAPI.js
+++ OmegleAPI.js
@@ -11,6 +11,9 @@ const getInputArea = () =>
 const getSendButton = () =>
     document.querySelector(".sendbtn");
 
+const getNextButton = () =>
+    document.querySelector(".disconnectbtn");
+
 const getAllMessages = () =>
     getAllLogs().map( element => [ element.querySelector( ".msgsource"   )
                                  , element.querySelector( ".notranslate" ) ||
@@ -36,13 +39,13 @@ const getAllYourMessages = () =>
     getAllMessages().filter ( object => object?.You )
                     .map    ( object => object.You  );
 
-const hasChatEnded = () => {
+const isChatOver = () => {
     const systemMessages = getAllSystemMessages();
-    return systemMessages.slice(-3, 3)[0].contains("disconnected")
-        || systemMessages.slice(-2, 3)[0].contains("disconnected");
+    return systemMessages.includes("You have disconnected.")
+        || systemMessages.includes("Stranger has disconnected.");
 }
 
-const write = (text) => {
+const write = text => {
     getInputArea().value = text;
 }
 
@@ -50,6 +53,41 @@ const send = () => {
     getSendButton().click();
 }
 
-const sendMessage = message => {
+const say = message => {
     write(message); send();
+}
+
+const nextChat = () => {
+    getNextButton().click();
+}
+
+const confirmNextChat = () => {
+    nextChat();
+}
+
+const endChat = () => {
+    nextChat(); confirmNextChat();
+}
+
+const sleep = (ms) =>
+    new Promise(resolve => setTimeout(resolve, ms));
+
+const ask = async (question, waiting) => {
+    if (isChatOver()) return null;
+
+    say(question);
+
+    let last = getAllStrangerMessages().slice(-1)[0];
+    let curr = last;
+
+    while (curr == last)
+    {
+        curr = getAllStrangerMessages().slice(-1)[0];
+
+        if (waiting-- < 0) return null;
+
+        await sleep(1000);
+    }
+
+    return curr;
 }
